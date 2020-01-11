fef2fff1c480c3185ba82094ffc4f34881dcbe9d
hanseul <hsdeb11@gmail.com>
2020-01-12 00:15:19 +0900

    Ranking.py 오타 수정

--- Ranking.py
+++ Ranking.py
@@ -7,17 +7,17 @@ class Ranking(object):
         self.sentence_tokenize = SentenceTokenizer()
 
         self.sentences = self.sentence_tokenize.text_to_sentence(text)
-        self.words = self.sentence_tokenize.get_wrods(self.sentences)
+        self.words = self.sentence_tokenize.get_words(self.sentences)
 
         self.graph_matrix = GraphMatrix()
         self.sentence_graph = self.graph_matrix.create_sentence_graph(self.words)
         self.words_graph, self.idx2word = self.graph_matrix.create_words_graph(self.words)
 
         self.textRank = TextRank()
-        self.sentence_rank_idx = self.textRank.get_ranks(self.sentence_graph)
+        self.sentence_rank_idx = self.textRank.get_rank(self.sentence_graph)
         self.sorted_sent_rank_idx = sorted(self.sentence_rank_idx, key=lambda k: self.sentence_rank_idx[k], reverse=True)
         
-        self.word_rank_idx = self.textRank.get_ranks(self.words_graph)
+        self.word_rank_idx = self.textRank.get_rank(self.words_graph)
         self.sorted_word_rank_idx = sorted(self.word_rank_idx, key=lambda k: self.word_rank_idx[k], reverse=True)
         
     def keywords(self, word_num=10):
